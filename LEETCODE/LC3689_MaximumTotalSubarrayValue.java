47bc85819f048dc8c905faf6af69571bd9cd196e
Swadhin  Dibya Jyoti <jyotiswadhindibya@gmail.com>
2026-06-10 00:30:36 +0530

    Solving Leetcode

---
+++ LEETCODE/LC3689_MaximumTotalSubarrayValue.java
@@ -0,0 +1,30 @@
+/*
+3689. Maximum Total Subarray Value I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums of length n and an integer k.
+
+You need to choose exactly k non-empty subarrays nums[l..r] of nums. Subarrays may overlap, and the exact same subarray (same l and r) can be chosen more than once.
+
+The value of a subarray nums[l..r] is defined as: max(nums[l..r]) - min(nums[l..r]).
+
+The total value is the sum of the values of all chosen subarrays.
+
+Return the maximum possible total value you can achieve.
+*/
+
+//Solution in java
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int n=nums.length,max=nums[0],min=nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i]>max) max=nums[i];
+            if(nums[i]<min) min=nums[i];
+        }
+        return 1l*(max-min)*k;
+    }
+}
