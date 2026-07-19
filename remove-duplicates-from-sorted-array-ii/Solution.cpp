3a88781fdf3c78f65b0fdd1dc24343b6c0eb8e56
Swadhin  Dibya Jyoti <jyotiswadhindibya@gmail.com>
2026-07-19 18:33:00 +0530

    Add solution for remove-duplicates-from-sorted-array-ii [cpp]

---
+++ remove-duplicates-from-sorted-array-ii/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size(),k=2;
+        if(n<=2) return n;
+        for(int i=2;i<n;i++)
+            if(nums[k-2]!=nums[i]) nums[k++]=nums[i];
+            return k;
+    }
+};
